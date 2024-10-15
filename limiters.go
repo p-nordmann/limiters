2786db5667ed1568b554ecbd3b68aa20ddcbf68e
p-nordmann <pierrelouis.nordmann@gmail.com>
2024-10-16 01:30:59 +0200

    wip: added interface and empty files

--- limiters.go
+++ limiters.go
@@ -5,3 +5,7 @@ import "context"
 type Limiter interface {
 	Limit(ctx context.Context) error
 }
+
+type Scheduler interface {
+	Schedule(ctx context.Context, f func(ctx context.Context) error) error
+}
